b922cab2a45366bbcf52c4fdccabec457dd567dd
gowsik <v.gowsik@gmail.com>
2020-07-25 10:03:51 +0530

    initialize in ubuntu

--- config/db.js
+++ config/db.js
@@ -3,21 +3,20 @@ const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        })
-        
-        console.log("MongoDB connected...!");
-    } catch (err) {
-        console.log(err);
-        // Exit process with failure
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+
+    console.log("MongoDB connected...!");
+  } catch (err) {
+    console.log(err);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
-
