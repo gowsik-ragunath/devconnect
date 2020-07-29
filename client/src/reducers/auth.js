efc04780cfd399b9016db6535952c35fa092669b
gowsik <v.gowsik@gmail.com>
2020-07-29 23:53:07 +0530

    Added logic to add token after registration

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAILED } from '../actions/types'
+import { REGISTER_SUCCESS, REGISTER_FAILED, USER_LOADED, AUTH_ERROR } from '../actions/types'
 
 const initialState = {
     token: localStorage.getItem("token"),
@@ -11,21 +11,31 @@ export default function(state = initialState, action) {
     const { type, payload } = action;
 
     switch(type) {
-        case REGISTER_SUCCESS:
-            localStorage.setItem("token", payload.token);
-            return {
-                ...state,
-                ...payload,
-                isAuthenticated: true,
-                loading: false
-            }
+        case USER_LOADED:
+					return {
+						...state,
+						isAuthenticated: true,
+						loading: false,
+						user: payload
+					}
+				case REGISTER_SUCCESS:
+					localStorage.setItem('token', payload.token);
+					return {
+							...state,
+							...payload,
+							isAuthenticated: true,
+							loading: false
+					}
         case REGISTER_FAILED:
-            localStorage.removeItem("token");
-            return {
-                ...state,
-                token: null,
-                isAuthenticated: false,
-                loading: false
-            }
+				case AUTH_ERROR:
+					localStorage.removeItem('token');
+					return {
+							...state,
+							token: null,
+							isAuthenticated: false,
+							loading: false
+					}
+				default:
+					return state;
     }
 }
