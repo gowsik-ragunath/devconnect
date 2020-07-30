3b0d83730b9a67fdf412f25cf2342346f7e63115
gowsik <v.gowsik@gmail.com>
2020-07-30 22:13:41 +0530

    Fixed the issue in loading auth reducer and added login actions

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,7 +1,14 @@
-import { REGISTER_SUCCESS, REGISTER_FAILED, USER_LOADED, AUTH_ERROR } from '../actions/types'
+import { 
+	REGISTER_SUCCESS, 
+	REGISTER_FAILED, 
+	USER_LOADED, 
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL
+} from '../actions/types';
 
 const initialState = {
-    token: localStorage.getItem("token"),
+    token: localStorage.getItem('token'),
     isAuthenticated: null,
     loading: true,
     user: null
@@ -11,31 +18,35 @@ export default function(state = initialState, action) {
     const { type, payload } = action;
 
     switch(type) {
-        case USER_LOADED:
-					return {
+			case USER_LOADED:
+				return {
+					...state,
+					isAuthenticated: true,
+					loading: false,
+					user: payload
+				};
+			case REGISTER_SUCCESS:
+			case LOGIN_SUCCESS:
+				console.log(payload);
+				localStorage.setItem('token', payload.token);
+				return {
 						...state,
+						...payload,
 						isAuthenticated: true,
-						loading: false,
-						user: payload
-					}
-				case REGISTER_SUCCESS:
-					localStorage.setItem('token', payload.token);
-					return {
-							...state,
-							...payload,
-							isAuthenticated: true,
-							loading: false
-					}
-        case REGISTER_FAILED:
-				case AUTH_ERROR:
-					localStorage.removeItem('token');
-					return {
-							...state,
-							token: null,
-							isAuthenticated: false,
-							loading: false
-					}
-				default:
-					return state;
+						loading: false
+				};
+			case REGISTER_FAILED:
+			case AUTH_ERROR:
+			case LOGIN_FAIL:
+				localStorage.removeItem('token');
+				return {
+						...state,
+						token: null,
+						isAuthenticated: false,
+						loading: false
+				};
+			default:
+				console.log("!!!!!!!!!!!!!!!!!!")
+				return state;
     }
 }
