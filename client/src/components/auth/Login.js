b373b6b9966822f303c6f79e20a03f26b045a5b5
gowsik <v.gowsik@gmail.com>
2020-08-03 21:01:05 +0530

    Added dashboard with private routes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -27,7 +27,7 @@ const Login = ({ login, isAuthenticated }) => {
 
     return (
         <Fragment>
-            <h1 className="large text-primary">Sign Up</h1>
+            <h1 className="large text-primary">Sign In</h1>
             <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
             <form className="form" onSubmit={ e => onSubmit(e) }>
                 <div className="form-group">
@@ -49,7 +49,7 @@ const Login = ({ login, isAuthenticated }) => {
                     onChange={e => onChange(e)} 
                 />
                 </div>
-                <input type="submit" className="btn btn-primary" value="Register" />
+                <input type="submit" className="btn btn-primary" value="Login" />
             </form>
             <p className="my-1">
                 Don't have an account? <Link to="/register">Register</Link>
