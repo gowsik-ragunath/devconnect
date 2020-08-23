be7c83073ee7bc56e27cc0e376e5a8069ad8966e
gowsik <v.gowsik@gmail.com>
2020-08-23 15:18:58 +0530

    Added logics to add user profile info from profile form

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
 	const [formData, setFormData] = useState({
       company: '',
       location: '',
@@ -40,6 +42,11 @@ const CreateProfile = props => {
 		[e.target.name]: e.target.value
 	});
 
+	const onSubmit = e => {
+		e.preventDefault();
+		createProfile(formData, history)
+	};
+
 	return (
 		<Fragment>
 			<h1 className="large text-primary">
@@ -50,7 +57,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -164,7 +171,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+	createProfile: PropTypes.func.isRequired,
 }
 
-export default CreateProfile
+export default connect(null, { createProfile })(withRouter(CreateProfile));
