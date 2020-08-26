9f1b761edc59a51f8d7149d97fbd2c2e6cd79372
gowsik <v.gowsik@gmail.com>
2020-08-26 21:06:59 +0530

    Added get profiles and get github url reducer and actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import { setAlert } from './alert';
 
 import { 
-  GET_PROFILE,
+	GET_PROFILE,
+	GET_PROFILES,
 	PROFILE_ERROR,
 	UPDATE_PROFILE,
 	ACCOUNT_DELETED,
-	CLEAR_PROFILE
+	CLEAR_PROFILE,
+	GET_REPOS
 } from './types';
 
 // Get current user profile
@@ -16,6 +18,63 @@ export const getCurrentProfile = () => async dispatch => {
         
         dispatch({
             type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch(err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+} 
+
+// Get all user profiles
+export const getProfiles = () => async dispatch => {
+	dispatch({type: CLEAR_PROFILE})
+
+    try {
+        const res = await axios.get('api/profile');
+        
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch(err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+} 
+
+// Get profile by ID
+export const getProfileById = () => async dispatch => {
+    try {
+        const res = await axios.get(`api/profile/github/${username}`);
+        
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        });
+    } catch(err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+	dispatch({
+		type: CLEAR_PROFILE
+	})
+
+    try {
+        const res = await axios.get('api/profile');
+        
+        dispatch({
+            type: GET_PROFILES,
             payload: res.data
         });
     } catch(err) {
